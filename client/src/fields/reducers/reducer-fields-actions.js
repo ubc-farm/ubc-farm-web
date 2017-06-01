e35ebc8b4be9c86bb5a7d23fd4efd9fe3400d8c1
Xingyu96 <taoxingyu@gmail.com>
2017-05-31 17:14:58 -0700

    deleted field now dispatches correct actions

--- client/src/fields/reducers/reducer-fields-actions.js
+++ client/src/fields/reducers/reducer-fields-actions.js
@@ -1,13 +1,17 @@
 /**
  * Created by Xingyu on 5/25/2017.
  */
-export default function(state=[], action={}){
+import {FIELD_DELETED} from "../actions/delete-field.js"
+
+export default function selectedField(state=[], action={}){
     switch(action.type) {
         case "FIELD_SELECTED":
-            console.log('event fired');
             return action.payload;
             break;
-
+        case FIELD_DELETED:
+            console.log("field delete trigger select");
+            return state = [];
+            break;
         default:
             return state;
     }
