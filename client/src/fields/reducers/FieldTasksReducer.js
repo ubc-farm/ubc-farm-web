b6ab809887a6233a1ba53f84ab47e48bf3ce10f6
Xingyu96 <taoxingyu@gmail.com>
2017-06-08 11:03:02 -0700

    field task list auto refreshes, added lines in FieldTaskReducer

--- client/src/fields/reducers/FieldTasksReducer.js
+++ client/src/fields/reducers/FieldTasksReducer.js
@@ -1,7 +1,9 @@
 /**
  * Created by Xingyu on 6/5/2017.
  */
-import {SET_TASKS_BYFIELD} from "../actions/fetchTaskByField"
+import {SET_TASKS_BYFIELD} from "../actions/fetchTaskByField";
+import {ADD_TASK} from "../../tasks/actions/save-task";
+import {TASK_DELETED} from "../../tasks/actions/delete-task";
 
 export default function fieldTasks(state=[], action={}){
     switch(action.type) {
@@ -9,6 +11,20 @@ export default function fieldTasks(state=[], action={}){
             console.log("set tasks by field!");
             return action.tasks;
             break;
+
+        case ADD_TASK:
+            console.log("TASK added! reducer...");
+            return[
+                ...state,
+                action.task
+            ];
+            break;
+
+        case TASK_DELETED:
+            console.log("field delete!");
+            return state.filter(item => item._id !== action.taskId);
+            break;
+
 
         default:
             return state;
