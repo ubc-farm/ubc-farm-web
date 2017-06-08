0fba14e232c22cea91949e98a61785bf707a251c
Xingyu96 <taoxingyu@gmail.com>
2017-06-08 01:01:35 -0700

    added radar module with correct loading sequence

--- client/src/fields/Radar.js
+++ client/src/fields/Radar.js
@@ -4,13 +4,29 @@
 import React, { Component } from 'react';
 
 class Radar extends Component {
+    componentDidMount() {
+        //fetch fields from database
+    }
+
+    // Constructor is responsible for setting up props and setting initial state
+    constructor(props) {
+        // Pass props to the parent component
+        super(props);
+        // Set initial state
+        this.state = {
+            // State needed
+        };
+
+    }
+
+
     render() {
         return (
             <div style={{ height:'30%'}}>
                 <iframe
                     name="iframe"
-                    src="https://weather.gc.ca/radar/index_e.html?id=wuj"
-                    style={{marginTop: 20}}
+                    src="https://weather.gc.ca/radar/index_e.html?id=wuj #animation"
+                    style={{marginTop: 20, width: '100%', height: '100%'}}
                 ></iframe>
             </div>
         );
